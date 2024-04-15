f4133357b0ce18dbe2195f2a1e1f887b969eacb0
zjun <565061763@qq.com>
2024-04-15 19:10:55 +0800

    FSuspendList

--- lib/src/main/java/com/sd/lib/list/FSuspendList.kt
+++ lib/src/main/java/com/sd/lib/list/FSuspendList.kt
@@ -1,6 +1,7 @@
 package com.sd.lib.list
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
@@ -28,56 +29,71 @@ interface FSuspendList<T> {
 
     /**
      * 添加数据
+     *
+     * @return true-本次调用数据发生了变化
      */
-    suspend fun add(data: T)
+    suspend fun add(data: T): Boolean
 
     /**
-     * 添加数据
+     * 添加数据并根据[distinct]去重，删除[FList]中重复的数据
+     *
+     * @param list 新数据
+     * @param distinct null-不进行去重操作，[distinct]返回true表示数据重复
+     * @return true-本次调用数据发生了变化
      */
-    suspend fun addAll(list: List<T>)
-
-    /**
-     * 添加数据并去重，删除[FSuspendList]中重复的数据
-     */
-    suspend fun addAllDistinct(
+    suspend fun addAll(
         list: List<T>,
         /** 去重条件，返回true表示数据重复 */
-        distinct: (oldItem: T, newItem: T) -> Boolean = { oldItem, newItem -> oldItem == newItem },
-    )
+        distinct: ((oldItem: T, newItem: T) -> Boolean)?,
+    ): Boolean
 
     /**
-     * 添加数据并去重，删除[list]中重复的数据
+     * 添加数据并根据[distinct]去重，删除[list]中重复的数据
+     *
+     * @param list 新数据
+     * @param distinct [distinct]返回true表示数据重复
      */
     suspend fun addAllDistinctInput(
         list: List<T>,
         /** 去重条件，返回true表示数据重复 */
-        distinct: (oldItem: T, newItem: T) -> Boolean = { oldItem, newItem -> oldItem == newItem },
-    )
+        distinct: (oldItem: T, newItem: T) -> Boolean,
+    ): Boolean
 
     /**
      * 如果[block]返回的新对象 != 原对象，则用新对象替换原对象并结束遍历
+     *
+     * @return true-本次调用数据发生了变化
      */
-    suspend fun replaceFirst(block: (T) -> T)
+    suspend fun replaceFirst(block: (T) -> T): Boolean
 
     /**
      * 如果[block]返回的新对象 != 原对象，则用新对象替换原对象
+     *
+     * @return true-本次调用数据发生了变化
      */
-    suspend fun replaceAll(block: (T) -> T)
+    suspend fun replaceAll(block: (T) -> T): Boolean
 
     /**
      * 删除第一个[predicate]为true的数据
+     *
+     * @return true-本次调用数据发生了变化
      */
-    suspend fun removeFirst(predicate: (T) -> Boolean)
+    suspend fun removeFirst(predicate: (T) -> Boolean): Boolean
 
     /**
      * 删除所有[predicate]为true的数据
+     *
+     * @return true-本次调用数据发生了变化
      */
-    suspend fun removeAll(predicate: (T) -> Boolean)
+    suspend fun removeAll(predicate: (T) -> Boolean): Boolean
 
     /**
      * 修改数据
+     *
+     * @param block [block]返回true表示数据修改成功
+     * @return true-本次调用数据发生了变化
      */
-    suspend fun modify(block: (list: MutableList<T>) -> Boolean)
+    suspend fun modify(block: (list: MutableList<T>) -> Boolean): Boolean
 }
 
 /**
@@ -104,146 +120,101 @@ private class FSuspendListImpl<T>(
     private val dispatcher: CoroutineDispatcher,
 ) : FSuspendList<T> {
 
-    private val _list: MutableList<T> = mutableListOf<T>().apply { this.addAll(initial) }
+    private val _list = FList(initial)
     private val _dataFlow: MutableStateFlow<List<T>> = MutableStateFlow(initial)
 
     override val data: List<T> get() = _dataFlow.value
     override val dataFlow: Flow<List<T>> = _dataFlow.asStateFlow()
 
     override suspend fun set(list: List<T>) {
-        modify { listData ->
-            listData.clear()
-            listData.addAll(list)
+        modifyList {
+            it.set(list)
             true
         }
     }
 
     override suspend fun clear() {
-        modify { listData ->
-            listData.clear()
+        modifyList {
+            it.clear()
             true
         }
     }
 
-    override suspend fun add(data: T) {
-        modify { listData ->
-            listData.add(data)
+    override suspend fun add(data: T): Boolean {
+        return modifyList {
+            it.add(data)
         }
     }
 
-    override suspend fun addAll(list: List<T>) {
-        if (list.isEmpty()) return
-        modify { listData ->
-            listData.addAll(list)
-        }
-    }
-
-    override suspend fun addAllDistinct(
+    override suspend fun addAll(
         list: List<T>,
-        distinct: (oldItem: T, newItem: T) -> Boolean,
-    ) {
-        if (list.isEmpty()) return
-        modify { listData ->
-            listData.removeAll { oldItem ->
-                list.firstOrNull { newItem ->
-                    distinct(oldItem, newItem)
-                } != null
-            }
-            listData.addAll(list)
+        distinct: ((oldItem: T, newItem: T) -> Boolean)?,
+    ): Boolean {
+        return modifyList {
+            it.addAll(
+                list = list,
+                distinct = distinct,
+            )
         }
     }
 
     override suspend fun addAllDistinctInput(
         list: List<T>,
         distinct: (oldItem: T, newItem: T) -> Boolean,
-    ) {
-        if (list.isEmpty()) return
-        modify { listData ->
-            val mutableList = list.toMutableList()
-            mutableList.removeAll { newItem ->
-                listData.firstOrNull { oldItem ->
-                    distinct(oldItem, newItem)
-                } != null
-            }
-            listData.addAll(mutableList)
+    ): Boolean {
+        return modifyList {
+            it.addAllDistinctInput(
+                list = list,
+                distinct = distinct,
+            )
         }
     }
 
-    override suspend fun replaceFirst(block: (T) -> T) {
-        modify { listData ->
-            var result = false
-            for (index in listData.indices) {
-                val item = listData[index]
-                val newItem = block(item)
-                if (newItem != item) {
-                    listData[index] = newItem
-                    result = true
-                    break
-                }
-            }
-            result
+    override suspend fun replaceFirst(block: (T) -> T): Boolean {
+        return modifyList {
+            it.replaceFirst(block)
         }
     }
 
-    override suspend fun replaceAll(block: (T) -> T) {
-        modify { listData ->
-            var result = false
-            for (index in listData.indices) {
-                val item = listData[index]
-                val newItem = block(item)
-                if (newItem != item) {
-                    listData[index] = newItem
-                    result = true
-                }
-            }
-            result
+    override suspend fun replaceAll(block: (T) -> T): Boolean {
+        return modifyList {
+            it.replaceAll(block)
         }
     }
 
-    override suspend fun removeFirst(predicate: (T) -> Boolean) {
-        modify { listData ->
-            listData.removeFirst(predicate)
+    override suspend fun removeFirst(predicate: (T) -> Boolean): Boolean {
+        return modifyList {
+            it.removeFirst(predicate)
         }
     }
 
-    override suspend fun removeAll(predicate: (T) -> Boolean) {
-        modify { listData ->
-            listData.removeAll(predicate)
+    override suspend fun removeAll(predicate: (T) -> Boolean): Boolean {
+        return modifyList {
+            it.removeAll(predicate)
         }
     }
 
-    override suspend fun modify(block: (list: MutableList<T>) -> Boolean) {
-        withContext(dispatcher) {
-            val oldSize = _list.size
-            if (block(_list) || oldSize != _list.size) {
-                ModifyResult(list = _list.toList(), oldSize = oldSize)
+    override suspend fun modify(block: (list: MutableList<T>) -> Boolean): Boolean {
+        return modifyList { list ->
+            val mutableList = list.data.toMutableList()
+            val oldSize = mutableList.size
+            block(mutableList) || mutableList.size != oldSize
+        }
+    }
+
+    private suspend fun modifyList(block: (list: FList<T>) -> Boolean): Boolean {
+        return dispatch {
+            val oldSize = _list.data.size
+            if (block(_list) || _list.data.size != oldSize) {
+                _dataFlow.value = _list.data.toList()
+                true
             } else {
-                null
-            }
-        }?.also { result ->
-            _dataFlow.value = result.list
-        }
-    }
-}
-
-private data class ModifyResult<T>(
-    val list: List<T>,
-    val oldSize: Int,
-)
-
-/**
- * 根据条件移除元素
- */
-private fun <T> MutableList<T>.removeFirst(
-    predicate: (T) -> Boolean,
-): Boolean {
-    with(iterator()) {
-        while (hasNext()) {
-            if (predicate(next())) {
-                remove()
-                return true
+                false
             }
         }
     }
-    return false
+
+    private suspend fun <T> dispatch(block: suspend CoroutineScope.() -> T): T {
+        return withContext(dispatcher, block)
+    }
 }
