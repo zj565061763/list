d7bd3091ec145ea96f6342a5cac18d553660c8fe
zjun <565061763@qq.com>
2024-05-03 23:26:07 +0800

    test

--- app/src/test/java/com/sd/demo/list/FSuspendListTest.kt
+++ app/src/test/java/com/sd/demo/list/FSuspendListTest.kt
@@ -1,7 +1,6 @@
 package com.sd.demo.list
 
 import com.sd.lib.list.FSuspendList
-import junit.framework.TestCase
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -12,19 +11,28 @@ class FSuspendListTest {
         val list = FSuspendList<Int>()
 
         listOf(1, 2, 3).let { data ->
-            list.set(data).also { assertEquals(true, it) }
-            assertEquals(data, list.data)
+            list.set(data).also {
+                assertEquals(true, it)
+                assertEquals(data, list.data)
+            }
         }
 
         listOf(4, 5, 6).let { data ->
-            list.set(data).also { assertEquals(true, it) }
-            assertEquals(data, list.data)
+            list.set(data).also {
+                assertEquals(true, it)
+                assertEquals(data, list.data)
+            }
         }
 
         emptyList<Int>().let { data ->
-            list.set(data).also { TestCase.assertEquals(true, it) }
-            list.set(data).also { TestCase.assertEquals(false, it) }
-            TestCase.assertEquals(data, list.data)
+            list.set(data).also {
+                assertEquals(true, it)
+                assertEquals(data, list.data)
+            }
+            list.set(data).also {
+                assertEquals(false, it)
+                assertEquals(data, list.data)
+            }
         }
     }
 
@@ -32,137 +40,309 @@ class FSuspendListTest {
     fun `test clear`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
 
-        list.set(listOf(1, 2, 3))
-        list.clear().also { assertEquals(true, it) }
-        list.clear().also { assertEquals(false, it) }
+        list.set(listOf(1, 2, 3)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
 
-        assertEquals(emptyList<Int>(), list.data)
+        list.clear().also {
+            assertEquals(true, it)
+            assertEquals(emptyList<Int>(), list.data)
+        }
+
+        list.clear().also {
+            assertEquals(false, it)
+            assertEquals(emptyList<Int>(), list.data)
+        }
     }
 
     @Test
     fun `test add`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
 
-        list.add(1).also { assertEquals(true, it) }
-        assertEquals(listOf(1), list.data)
+        list.add(1).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1), list.data)
+        }
 
-        list.add(2).also { assertEquals(true, it) }
-        assertEquals(listOf(1, 2), list.data)
+        list.add(2).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2), list.data)
+        }
     }
 
     @Test
     fun `test addAll`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 2, 3)).also { assertEquals(true, it) }
-        list.addAll(listOf(1, 2, 4)).also { assertEquals(true, it) }
-        assertEquals(listOf(3, 1, 2, 4), list.data)
+
+        list.addAll(listOf(1, 2, 3)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
+
+        list.addAll(listOf(1, 2, 4)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(3, 1, 2, 4), list.data)
+        }
+
+        list.addAll(emptyList()).also {
+            assertEquals(false, it)
+            assertEquals(listOf(3, 1, 2, 4), list.data)
+        }
     }
 
     @Test
     fun `test addAll none distinct`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 2, 3)).also { assertEquals(true, it) }
-        list.addAll(listOf(1, 2, 4), distinct = null).also { assertEquals(true, it) }
-        assertEquals(listOf(1, 2, 3, 1, 2, 4), list.data)
+
+        list.addAll(listOf(1, 2, 3)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
+
+        list.addAll(listOf(1, 2, 4), distinct = null).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3, 1, 2, 4), list.data)
+        }
+
+        list.addAll(emptyList(), distinct = null).also {
+            assertEquals(false, it)
+            assertEquals(listOf(1, 2, 3, 1, 2, 4), list.data)
+        }
     }
 
     @Test
     fun `test addAllDistinctInput`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 2, 3)).also { assertEquals(true, it) }
-        list.addAllDistinctInput(listOf(1, 2, 4)).also { assertEquals(true, it) }
-        assertEquals(listOf(1, 2, 3, 4), list.data)
+
+        list.addAll(listOf(1, 2, 3)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
+
+        list.addAllDistinctInput(listOf(1, 2, 4)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3, 4), list.data)
+        }
+
+        list.addAllDistinctInput(emptyList()).also {
+            assertEquals(false, it)
+            assertEquals(listOf(1, 2, 3, 4), list.data)
+        }
     }
 
     @Test
     fun `test addAllDistinctInput none distinct`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 2, 3)).also { assertEquals(true, it) }
-        list.addAllDistinctInput(listOf(1, 2, 4), distinct = null).also { assertEquals(true, it) }
-        assertEquals(listOf(1, 2, 3, 1, 2, 4), list.data)
+
+        list.addAll(listOf(1, 2, 3)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
+
+        list.addAllDistinctInput(listOf(1, 2, 4), distinct = null).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3, 1, 2, 4), list.data)
+        }
+
+        list.addAllDistinctInput(emptyList(), distinct = null).also {
+            assertEquals(false, it)
+            assertEquals(listOf(1, 2, 3, 1, 2, 4), list.data)
+        }
     }
 
     @Test
     fun `test replaceFirst`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 1, 1)).also { assertEquals(true, it) }
 
-        list.replaceFirst { if (it == 1) 0 else it }.also { assertEquals(true, it) }
-        assertEquals(listOf(0, 1, 1), list.data)
+        list.addAll(listOf(1, 1, 1)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 1, 1), list.data)
+        }
 
-        list.replaceFirst { if (it == 1) 0 else it }.also { assertEquals(true, it) }
-        assertEquals(listOf(0, 0, 1), list.data)
+        list.replaceFirst { if (it == 1) 0 else it }.also {
+            assertEquals(true, it)
+            assertEquals(listOf(0, 1, 1), list.data)
+        }
+
+        list.replaceFirst { if (it == 1) 0 else it }.also {
+            assertEquals(true, it)
+            assertEquals(listOf(0, 0, 1), list.data)
+        }
+
+        list.replaceFirst { if (it == 100) 0 else it }.also {
+            assertEquals(false, it)
+            assertEquals(listOf(0, 0, 1), list.data)
+        }
     }
 
     @Test
     fun `test replaceAll`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 1, 1)).also { assertEquals(true, it) }
-        list.replaceAll { if (it == 1) 0 else it }.also { assertEquals(true, it) }
-        assertEquals(listOf(0, 0, 0), list.data)
+
+        list.addAll(listOf(1, 1, 1)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 1, 1), list.data)
+        }
+
+        list.replaceAll { if (it == 1) 0 else it }.also {
+            assertEquals(true, it)
+            assertEquals(listOf(0, 0, 0), list.data)
+        }
+
+        list.replaceAll { if (it == 100) 0 else it }.also {
+            assertEquals(false, it)
+            assertEquals(listOf(0, 0, 0), list.data)
+        }
     }
 
     @Test
     fun `test removeFirst`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 1, 1)).also { assertEquals(true, it) }
 
-        list.removeFirst { it == 1 }.also { assertEquals(true, it) }
-        assertEquals(listOf(1, 1), list.data)
+        list.addAll(listOf(1, 1, 1)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 1, 1), list.data)
+        }
 
-        list.removeFirst { it == 1 }.also { assertEquals(true, it) }
-        assertEquals(listOf(1), list.data)
+        list.removeFirst { it == 1 }.also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 1), list.data)
+        }
+
+        list.removeFirst { it == 1 }.also {
+            assertEquals(true, it)
+            assertEquals(listOf(1), list.data)
+        }
+
+        list.removeFirst { it == 100 }.also {
+            assertEquals(false, it)
+            assertEquals(listOf(1), list.data)
+        }
     }
 
     @Test
     fun `test removeAll`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 1, 1)).also { assertEquals(true, it) }
-        list.removeAll { it == 1 }.also { assertEquals(true, it) }
-        assertEquals(emptyList<Int>(), list.data)
+
+        list.addAll(listOf(1, 1, 1)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 1, 1), list.data)
+        }
+
+        list.removeAll { it == 100 }.also {
+            assertEquals(false, it)
+            assertEquals(listOf(1, 1, 1), list.data)
+        }
+
+        list.removeAll { it == 1 }.also {
+            assertEquals(true, it)
+            assertEquals(emptyList<Int>(), list.data)
+        }
     }
 
     @Test
     fun `test insert`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.insert(0, 3).also { assertEquals(true, it) }
-        list.insert(0, 2).also { assertEquals(true, it) }
-        list.insert(0, 1).also { assertEquals(true, it) }
-        assertEquals(listOf(1, 2, 3), list.data)
 
-        list.insert(1, 0)
-        assertEquals(listOf(1, 0, 2, 3), list.data)
+        list.insert(0, 3).also {
+            assertEquals(true, it)
+            assertEquals(listOf(3), list.data)
+        }
+
+        list.insert(0, 2).also {
+            assertEquals(true, it)
+            assertEquals(listOf(2, 3), list.data)
+        }
+
+        list.insert(0, 1).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
+
+        list.insert(1, 0).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 0, 2, 3), list.data)
+        }
     }
 
     @Test
     fun `test insertAll`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 2, 3)).also { assertEquals(true, it) }
-        list.insertAll(0, listOf(1, 2, 4)).also { assertEquals(true, it) }
-        assertEquals(listOf(1, 2, 4, 3), list.data)
+
+        list.addAll(listOf(1, 2, 3)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
+
+        list.insertAll(0, listOf(1, 2, 4)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 4, 3), list.data)
+        }
+
+        list.insertAll(0, emptyList()).also {
+            assertEquals(false, it)
+            assertEquals(listOf(1, 2, 4, 3), list.data)
+        }
     }
 
     @Test
     fun `test insertAll none distinct`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 2, 3)).also { assertEquals(true, it) }
-        list.insertAll(0, listOf(1, 2, 4), distinct = null).also { assertEquals(true, it) }
-        assertEquals(listOf(1, 2, 4, 1, 2, 3), list.data)
+
+        list.addAll(listOf(1, 2, 3)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
+
+        list.insertAll(0, listOf(1, 2, 4), distinct = null).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 4, 1, 2, 3), list.data)
+        }
+
+        list.insertAll(0, emptyList(), distinct = null).also {
+            assertEquals(false, it)
+            assertEquals(listOf(1, 2, 4, 1, 2, 3), list.data)
+        }
     }
 
     @Test
     fun `test insertAllDistinctInput`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 2, 3)).also { assertEquals(true, it) }
-        list.insertAllDistinctInput(0, listOf(1, 2, 4)).also { assertEquals(true, it) }
-        assertEquals(listOf(4, 1, 2, 3), list.data)
+
+        list.addAll(listOf(1, 2, 3)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
+
+        list.insertAllDistinctInput(0, listOf(1, 2, 4)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(4, 1, 2, 3), list.data)
+        }
+
+        list.insertAllDistinctInput(0, emptyList()).also {
+            assertEquals(false, it)
+            assertEquals(listOf(4, 1, 2, 3), list.data)
+        }
     }
 
     @Test
     fun `test insertAllDistinctInput none distinct`(): Unit = runBlocking {
         val list = FSuspendList<Int>()
-        list.addAll(listOf(1, 2, 3)).also { assertEquals(true, it) }
-        list.insertAllDistinctInput(0, listOf(1, 2, 4), distinct = null).also { assertEquals(true, it) }
-        assertEquals(listOf(1, 2, 4, 1, 2, 3), list.data)
+
+        list.addAll(listOf(1, 2, 3)).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 3), list.data)
+        }
+
+        list.insertAllDistinctInput(0, listOf(1, 2, 4), distinct = null).also {
+            assertEquals(true, it)
+            assertEquals(listOf(1, 2, 4, 1, 2, 3), list.data)
+        }
+
+        list.insertAllDistinctInput(0, emptyList(), distinct = null).also {
+            assertEquals(false, it)
+            assertEquals(listOf(1, 2, 4, 1, 2, 3), list.data)
+        }
     }
 }
