bedf56fe2ad74ad1771dd10541c0aa12167a5a54
zjun <565061763@qq.com>
2024-12-18 18:40:33 +0800

    Simplify

--- lib/src/main/java/com/sd/lib/list/SynchronizedList.kt
+++ lib/src/main/java/com/sd/lib/list/SynchronizedList.kt
@@ -17,113 +17,49 @@ private class SynchronizedList<T>(
 
   private val _lock = lock ?: this
 
-  override fun getData(): List<T> {
-    return synchronized(_lock) {
-      proxy.getData()
-    }
-  }
+  override fun getData(): List<T> = synchronized(_lock) { proxy.getData() }
 
-  override fun set(elements: Collection<T>): Boolean {
-    return synchronized(_lock) {
-      proxy.set(elements)
-    }
-  }
+  override fun set(elements: Collection<T>): Boolean = synchronized(_lock) { proxy.set(elements) }
 
-  override fun clear(): Boolean {
-    return synchronized(_lock) {
-      proxy.clear()
-    }
-  }
+  override fun clear(): Boolean = synchronized(_lock) { proxy.clear() }
 
-  override fun add(element: T): Boolean {
-    return synchronized(_lock) {
-      proxy.add(element)
-    }
-  }
+  override fun add(element: T): Boolean = synchronized(_lock) { proxy.add(element) }
 
   override fun addAll(
     elements: Collection<T>,
     distinct: ((oldItem: T, newItem: T) -> Boolean)?,
-  ): Boolean {
-    return synchronized(_lock) {
-      proxy.addAll(elements, distinct)
-    }
-  }
+  ): Boolean = synchronized(_lock) { proxy.addAll(elements, distinct) }
 
   override fun addAllDistinctInput(
     elements: Collection<T>,
     distinct: ((oldItem: T, newItem: T) -> Boolean)?,
-  ): Boolean {
-    return synchronized(_lock) {
-      proxy.addAllDistinctInput(elements, distinct)
-    }
-  }
+  ): Boolean = synchronized(_lock) { proxy.addAllDistinctInput(elements, distinct) }
 
-  override fun replaceFirst(block: (T) -> T): Boolean {
-    return synchronized(_lock) {
-      proxy.replaceFirst(block)
-    }
-  }
+  override fun replaceFirst(block: (T) -> T): Boolean = synchronized(_lock) { proxy.replaceFirst(block) }
 
-  override fun replaceAll(block: (T) -> T): Boolean {
-    return synchronized(_lock) {
-      proxy.replaceAll(block)
-    }
-  }
+  override fun replaceAll(block: (T) -> T): Boolean = synchronized(_lock) { proxy.replaceAll(block) }
 
-  override fun replaceAt(index: Int, element: T): Boolean {
-    return synchronized(_lock) {
-      proxy.replaceAt(index, element)
-    }
-  }
+  override fun replaceAt(index: Int, element: T): Boolean = synchronized(_lock) { proxy.replaceAt(index, element) }
 
-  override fun removeFirst(predicate: (T) -> Boolean): Boolean {
-    return synchronized(_lock) {
-      proxy.removeFirst(predicate)
-    }
-  }
+  override fun removeFirst(predicate: (T) -> Boolean): Boolean = synchronized(_lock) { proxy.removeFirst(predicate) }
 
-  override fun removeAll(predicate: (T) -> Boolean): Boolean {
-    return synchronized(_lock) {
-      proxy.removeAll(predicate)
-    }
-  }
+  override fun removeAll(predicate: (T) -> Boolean): Boolean = synchronized(_lock) { proxy.removeAll(predicate) }
 
-  override fun removeAt(index: Int): Boolean {
-    return synchronized(_lock) {
-      proxy.removeAt(index)
-    }
-  }
+  override fun removeAt(index: Int): Boolean = synchronized(_lock) { proxy.removeAt(index) }
 
-  override fun insert(index: Int, element: T): Boolean {
-    return synchronized(_lock) {
-      proxy.insert(index, element)
-    }
-  }
+  override fun insert(index: Int, element: T): Boolean = synchronized(_lock) { proxy.insert(index, element) }
 
   override fun insertAll(
     index: Int,
     elements: Collection<T>,
     distinct: ((oldItem: T, newItem: T) -> Boolean)?,
-  ): Boolean {
-    return synchronized(_lock) {
-      proxy.insertAll(index, elements, distinct)
-    }
-  }
+  ): Boolean = synchronized(_lock) { proxy.insertAll(index, elements, distinct) }
 
   override fun insertAllDistinctInput(
     index: Int,
     elements: Collection<T>,
     distinct: ((oldItem: T, newItem: T) -> Boolean)?,
-  ): Boolean {
-    return synchronized(_lock) {
-      proxy.insertAllDistinctInput(index, elements, distinct)
-    }
-  }
+  ): Boolean = synchronized(_lock) { proxy.insertAllDistinctInput(index, elements, distinct) }
 
-  override fun <R> modify(block: FList<T>.() -> R): R {
-    return synchronized(_lock) {
-      block.invoke(this)
-    }
-  }
+  override fun <R> modify(block: FList<T>.() -> R): R = synchronized(_lock) { block.invoke(this) }
 }
