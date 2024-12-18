74c477aca14519ed5287fbc4da0d65e912d39708
zjun <565061763@qq.com>
2024-12-18 18:20:32 +0800

    Refactor notifyChange

--- lib/src/main/java/com/sd/lib/list/OnChangeList.kt
+++ lib/src/main/java/com/sd/lib/list/OnChangeList.kt
@@ -10,57 +10,57 @@ internal class OnChangeList<T>(
   }
 
   override fun set(elements: Collection<T>): Boolean {
-    return proxy.set(elements).also { if (it) onChange() }
+    return proxy.set(elements).also(::notifyChange)
   }
 
   override fun clear(): Boolean {
-    return proxy.clear().also { if (it) onChange() }
+    return proxy.clear().also(::notifyChange)
   }
 
   override fun add(element: T): Boolean {
-    return proxy.add(element).also { if (it) onChange() }
+    return proxy.add(element).also(::notifyChange)
   }
 
   override fun addAll(
     elements: Collection<T>,
     distinct: ((oldItem: T, newItem: T) -> Boolean)?,
   ): Boolean {
-    return proxy.addAll(elements, distinct).also { if (it) onChange() }
+    return proxy.addAll(elements, distinct).also(::notifyChange)
   }
 
   override fun addAllDistinctInput(
     elements: Collection<T>,
     distinct: ((oldItem: T, newItem: T) -> Boolean)?,
   ): Boolean {
-    return proxy.addAllDistinctInput(elements, distinct).also { if (it) onChange() }
+    return proxy.addAllDistinctInput(elements, distinct).also(::notifyChange)
   }
 
   override fun replaceFirst(block: (T) -> T): Boolean {
-    return proxy.replaceFirst(block).also { if (it) onChange() }
+    return proxy.replaceFirst(block).also(::notifyChange)
   }
 
   override fun replaceAll(block: (T) -> T): Boolean {
-    return proxy.replaceAll(block).also { if (it) onChange() }
+    return proxy.replaceAll(block).also(::notifyChange)
   }
 
   override fun replaceAt(index: Int, element: T): Boolean {
-    return proxy.replaceAt(index, element).also { if (it) onChange() }
+    return proxy.replaceAt(index, element).also(::notifyChange)
   }
 
   override fun removeFirst(predicate: (T) -> Boolean): Boolean {
-    return proxy.removeFirst(predicate).also { if (it) onChange() }
+    return proxy.removeFirst(predicate).also(::notifyChange)
   }
 
   override fun removeAll(predicate: (T) -> Boolean): Boolean {
-    return proxy.removeAll(predicate).also { if (it) onChange() }
+    return proxy.removeAll(predicate).also(::notifyChange)
   }
 
   override fun removeAt(index: Int): Boolean {
-    return proxy.removeAt(index).also { if (it) onChange() }
+    return proxy.removeAt(index).also(::notifyChange)
   }
 
   override fun insert(index: Int, element: T): Boolean {
-    return proxy.insert(index, element).also { if (it) onChange() }
+    return proxy.insert(index, element).also(::notifyChange)
   }
 
   override fun insertAll(
@@ -68,7 +68,7 @@ internal class OnChangeList<T>(
     elements: Collection<T>,
     distinct: ((oldItem: T, newItem: T) -> Boolean)?,
   ): Boolean {
-    return proxy.insertAll(index, elements, distinct).also { if (it) onChange() }
+    return proxy.insertAll(index, elements, distinct).also(::notifyChange)
   }
 
   override fun insertAllDistinctInput(
@@ -76,10 +76,14 @@ internal class OnChangeList<T>(
     elements: Collection<T>,
     distinct: ((oldItem: T, newItem: T) -> Boolean)?,
   ): Boolean {
-    return proxy.insertAllDistinctInput(index, elements, distinct).also { if (it) onChange() }
+    return proxy.insertAllDistinctInput(index, elements, distinct).also(::notifyChange)
   }
 
   override fun <R> modify(block: FList<T>.() -> R): R {
     return block.invoke(this)
+  }
+
+  private fun notifyChange(change: Boolean) {
+    if (change) onChange()
   }
 }
